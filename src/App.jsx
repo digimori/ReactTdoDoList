0aedcd8f6e3680f57b69ae8ab7d1acd80e985e72
digimori <hanaorikuu@gmail.com>
2023-07-03 13:11:27 +0000

    Completed basic function to add items to Todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useState } from "react"
+import "./styles.css"
+
 export default function App() {
-  return "Hi"
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+  }
+
+  return (
+  <>
+  <form onSubmit={handleSubmit} className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} // Whenever the input is changed, get the new value of the input and set that as the new value
+      type="text" id="item" />
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">Todo List</h1>
+  <ul className="list">
+    {todos.map(todo => {
+      return( <li>
+        <label>
+          <input type="checkbox" checked={todo.completed} />
+          {todo.title}
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      )}
+    )}
+  </ul>
+</>
+  )
 }
