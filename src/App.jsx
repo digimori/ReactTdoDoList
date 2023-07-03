d92d007a1a0966c30413adc9f67118b1dcd5b57f
digimori <hanaorikuu@gmail.com>
2023-07-03 13:32:01 +0000

    Delete button added and functional - Selects correct id on click

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,25 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false},
       ]
     })
+    setNewItem("")
+  }
+
+  function toggleToDo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed}
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -29,12 +48,13 @@ export default function App() {
   <h1 className="header">Todo List</h1>
   <ul className="list">
     {todos.map(todo => {
-      return( <li>
+      return( <li key={todo.id}>
         <label>
-          <input type="checkbox" checked={todo.completed} />
+          <input type="checkbox" checked={todo.completed} onChange={e => toggleToDo(todo.id, e.target.checked)} // This is checking if the toggle is checked, e=eventlistener
+          /> 
           {todo.title}
         </label>
-        <button className="btn btn-danger">Delete</button>
+        <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
       </li>
       )}
     )}
